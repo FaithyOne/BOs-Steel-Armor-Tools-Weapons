400acf78ddf344560c789ec729ee5b5fbfc53c8b
Stephanie <15845252+FaithyOne@users.noreply.github.com>
2021-11-15 22:01:47 +0100

    Steel weapons added

--- src/main/java/de/markusbordihn/steelarmortoolsweapons/item/ModItems.java
+++ src/main/java/de/markusbordihn/steelarmortoolsweapons/item/ModItems.java
@@ -22,6 +22,10 @@ package de.markusbordihn.steelarmortoolsweapons.item;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.SwordItem;
+import net.minecraft.world.item.ShovelItem;
+import net.minecraft.world.item.PickaxeItem;
+import net.minecraft.world.item.AxeItem;
+import net.minecraft.world.item.HoeItem;
 import net.minecraftforge.fmllegacy.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -32,17 +36,33 @@ import de.markusbordihn.steelarmortoolsweapons.Annotations.TemplateEntryPoint;
 
 public class ModItems {
 
-  protected ModItems() {
-
-  }
+  protected ModItems() {}
 
   public static final DeferredRegister<Item> ITEMS =
       DeferredRegister.create(ForgeRegistries.ITEMS, Constants.MOD_ID);
 
   @TemplateEntryPoint("Register Items")
+
   public static final RegistryObject<Item> STEEL_SWORD =
       ITEMS.register("steel_sword", () -> new SwordItem(ModTiers.STEEL, 3, -2.4F,
           (new Item.Properties()).tab(CreativeModeTab.TAB_COMBAT)));
+
+  public static final RegistryObject<Item> STEEL_SHOVEL =
+      ITEMS.register("steel_shovel", () -> new ShovelItem(ModTiers.STEEL, 1.5F, -3.0F,
+          (new Item.Properties()).tab(CreativeModeTab.TAB_TOOLS)));
+
+  public static final RegistryObject<Item> STEEL_PICKAXE =
+      ITEMS.register("steel_pickaxe", () -> new PickaxeItem(ModTiers.STEEL, 1, -2.8F,
+          (new Item.Properties()).tab(CreativeModeTab.TAB_TOOLS)));
+
+  public static final RegistryObject<Item> STEEL_AXE =
+      ITEMS.register("steel_axe", () -> new AxeItem(ModTiers.STEEL, 6.0F, -3.1F,
+          (new Item.Properties()).tab(CreativeModeTab.TAB_TOOLS)));
+
+  public static final RegistryObject<Item> STEEL_HOE =
+      ITEMS.register("steel_hoe", () -> new HoeItem(ModTiers.STEEL, -2, -1.0F,
+          (new Item.Properties()).tab(CreativeModeTab.TAB_TOOLS)));
+
 
   @TemplateEntryPoint("Register Block Items")
 
